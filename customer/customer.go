ec2fa38ca49c71dad2ec7722b70a4d94df0a43cc
Yudi Primaputra <xpartacvs@gmail.com>
2021-07-09 02:56:45 +0700

    Add customer.Delete func

--- customer/customer.go
+++ customer/customer.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"regexp"
 	"strconv"
 	"strings"
 
@@ -21,24 +20,59 @@ type Customer interface {
 	SignUp(regForm *RegistrationForm) error
 	ChangePassword(customerId, newPassword string) error
 	Details(customerIdOrEmail string) (CustomerDetail, error)
+	Delete(customerId string) error
+}
+
+func (c *customer) Delete(customerId string) error {
+	if !core.RgxNumber.MatchString(customerId) {
+		return core.ErrRcInvalidCredential
+	}
+
+	resp, err := c.core.CallApi(http.MethodPost, "customers", "delete", url.Values{"customer-id": {customerId}})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	boolResult, err := strconv.ParseBool(string(bytesResp))
+	if err != nil {
+		return err
+	}
+	if !boolResult {
+		return core.ErrRcOperationFailed
+	}
+
+	return nil
 }
 
 func (c *customer) Details(customerIdOrEmail string) (CustomerDetail, error) {
 	ret := CustomerDetail{}
 	data := url.Values{}
-	rgxEmail := regexp.MustCompile("^(?:(?:(?:(?:[a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(?:\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|(?:(?:\\x22)(?:(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(?:\\x20|\\x09)+)?(?:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(?:(?:(?:\\x20|\\x09)*(?:\\x0d\\x0a))?(\\x20|\\x09)+)?(?:\\x22))))@(?:(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(?:(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])(?:[a-zA-Z]|\\d|-|\\.|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*(?:[a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.?$")
-	rgxNumber := regexp.MustCompile("^[0-9]+$")
 
 	var funcName, query string
 	switch {
-	case rgxEmail.MatchString(customerIdOrEmail):
+	case core.RgxEmail.MatchString(customerIdOrEmail):
 		funcName = "details"
 		query = "username"
-	case rgxNumber.MatchString(customerIdOrEmail):
+	case core.RgxNumber.MatchString(customerIdOrEmail):
 		funcName = "details-by-id"
 		query = "customer-id"
 	default:
-		return ret, errors.New("invalid username or email")
+		return ret, core.ErrRcInvalidCredential
 	}
 	data.Add(query, customerIdOrEmail)
 
@@ -103,7 +137,7 @@ func (c *customer) ChangePassword(customerId, newPassword string) error {
 		return err
 	}
 	if !boolResult {
-		return errors.New("password unchanged")
+		return core.ErrRcOperationFailed
 	}
 
 	return nil
