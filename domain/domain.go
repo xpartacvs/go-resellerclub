3d482991d3b9f48a2736f9aed677ea48f99296db
Achmad Chun-Chun Winata Adi <anonychun@gmail.com>
2021-07-14 21:46:59 +0700

    Unfinished task

--- domain/domain.go
+++ domain/domain.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -110,16 +111,9 @@ func (d *domain) SearchOrders(criteria OrderCriteria) error {
 func (d *domain) SuggestNames(keyword, tldOnly string, exactMatch, adult bool) (SuggestNames, error) {
 	data := make(url.Values)
 	data.Add("keyword", keyword)
-	if exactMatch {
-		data.Add("exact-match", "True")
-	} else {
-		data.Add("exact-match", "False")
-	}
-	if adult {
-		data.Add("adult", "True")
-	} else {
-		data.Add("adult", "False")
-	}
+	data.Add("tld-only", tldOnly)
+	data.Add("exact-match", strconv.FormatBool(exactMatch))
+	data.Add("adult", strconv.FormatBool(adult))
 
 	resp, err := d.core.CallApi(http.MethodGet, "domains/v5", "suggest-names", data)
 	if err != nil {
@@ -148,4 +142,191 @@ func (d *domain) SuggestNames(keyword, tldOnly string, exactMatch, adult bool) (
 	}
 
 	return suggestNames, nil
+}
+
+// TODO
+// Missing: attr-name Map[name], attrValue Map[value]
+func (d *domain) Register(domainName string, years int, ns []string, customerID, regContactID, adminContactID, techContactID, billingContactID int, invoiceOption string, purchasePrivacy bool, protectPrivacy bool, autoRenew bool, discountAmount float64, purchasePremiumDNS bool) error {
+	// data := make(url.Values)
+	return nil
+}
+
+// TODO
+// Problems: can't get response for renew order-id
+func (d *domain) Renew(orderID, years, expDate int, purchasePrivacy, autoRenew bool, invoiceOption string, discountAmount float64, purchasePremiumDNS bool) error {
+	data := make(url.Values)
+	data.Add("order-id", strconv.Itoa(orderID))
+	data.Add("exp-date", strconv.Itoa(expDate))
+	data.Add("purchase-privacy", strconv.FormatBool(purchasePrivacy))
+	data.Add("auto-renew", strconv.FormatBool(autoRenew))
+	data.Add("invoice-option", invoiceOption)
+	data.Add("discount-amount", strconv.FormatFloat(discountAmount, 'f', 2, 64))
+	data.Add("purchase-premium-dns", strconv.FormatBool(purchasePremiumDNS))
+
+	resp, err := d.core.CallApi(http.MethodPost, "domains", "renew", data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	return nil
+}
+
+func (d *domain) GetCustomerDefaultNameServers(customerID int) error {
+	data := make(url.Values)
+	data.Add("customer-id", strconv.Itoa(customerID))
+
+	resp, err := d.core.CallApi(http.MethodGet, "domains", "customer-default-ns", data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	return nil
+}
+
+func (d *domain) GetOrderID(domainName string) error {
+	data := make(url.Values)
+	data.Add("domain-name", domainName)
+
+	resp, err := d.core.CallApi(http.MethodGet, "domains", "order-id", data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	return nil
+}
+
+func (d *domain) GetRegistrationOrderDetails(orderID int, options []string) error {
+	data := make(url.Values)
+	data.Add("order-id", strconv.Itoa(orderID))
+	for _, option := range options {
+		data.Add("options", option)
+	}
+
+	resp, err := d.core.CallApi(http.MethodGet, "domains", "details", data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	return nil
+}
+
+func (d *domain) ModifyNameServers(orderID int, ns []string) error {
+	data := make(url.Values)
+	data.Add("order-id", strconv.Itoa(orderID))
+	for _, n := range ns {
+		data.Add("ns", n)
+	}
+
+	resp, err := d.core.CallApi(http.MethodGet, "domains", "modify-ns", data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	return nil
+}
+
+func (d *domain) AddChildNameServer(orderID int, cns string, ips []string) error {
+	data := make(url.Values)
+	data.Add("order-id", strconv.Itoa(orderID))
+	data.Add("cns", cns)
+	for _, ip := range ips {
+		data.Add("ip", ip)
+	}
+
+	resp, err := d.core.CallApi(http.MethodGet, "domains", "add-cns", data)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return err
+		}
+		return errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	return nil
 }
