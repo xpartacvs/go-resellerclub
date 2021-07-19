45b2edb634ec985b65174249cc1fbeb03223366c
Yudi Primaputra <xpartacvs@gmail.com>
2021-07-19 19:26:10 +0700

    Remove QueryString func, Add and implement UrlValues func and CopyTo func

--- core/entity_attributes.go
+++ core/entity_attributes.go
@@ -14,7 +14,32 @@ type EntityAttributes interface {
 	Add(key, val string)
 	Get(key string) string
 	Del(key string)
-	QueryString() string
+	UrlValues() url.Values
+	CopyTo(dest *url.Values)
+}
+
+func (e *entityAttributes) CopyTo(dest *url.Values) {
+	if dest == nil {
+		return
+	}
+
+	wg := sync.WaitGroup{}
+	rwMutex := sync.RWMutex{}
+	index := 0
+
+	for key, val := range e.data {
+		wg.Add(1)
+		index++
+		go func(i int, k, v string) {
+			defer wg.Done()
+			rwMutex.Lock()
+			dest.Add("attr-name"+strconv.Itoa(i), k)
+			dest.Add("attr-value"+strconv.Itoa(i), v)
+			rwMutex.Unlock()
+		}(index, key, val)
+	}
+
+	wg.Wait()
 }
 
 func (e *entityAttributes) Add(key, val string) {
@@ -29,11 +54,7 @@ func (e *entityAttributes) Del(key string) {
 	delete(e.data, key)
 }
 
-func (e *entityAttributes) QueryString() string {
-	return e.urlValues().Encode()
-}
-
-func (e *entityAttributes) urlValues() url.Values {
+func (e *entityAttributes) UrlValues() url.Values {
 	ret := url.Values{}
 	wg := sync.WaitGroup{}
 	rwMutex := sync.RWMutex{}
