f7aa5a4fc7cf7121c8eace616ec74ebf80e13c28
Yudi Primaputra <xpartacvs@gmail.com>
2021-07-20 04:57:49 +0700

    Add contact.Details func

--- contact/contact.go
+++ contact/contact.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/xpartacvs/go-resellerclub/core"
@@ -16,6 +17,43 @@ type contact struct {
 
 type Contact interface {
 	Add(detail *ContactDetail, attributes core.EntityAttributes) error
+	Details(contactId string) (*ContactDetail, error)
+}
+
+func (c *contact) Details(contactId string) (*ContactDetail, error) {
+	if !core.RgxNumber.MatchString(contactId) {
+		return nil, core.ErrRcInvalidCredential
+	}
+
+	data := url.Values{}
+	data.Add("contact-id", contactId)
+
+	resp, err := c.core.CallApi(http.MethodGet, "contacts", "details", data)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		errResponse := core.JSONStatusResponse{}
+		err = json.Unmarshal(bytesResp, &errResponse)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(strings.ToLower(errResponse.Message))
+	}
+
+	ret := new(ContactDetail)
+	if err = json.Unmarshal(bytesResp, ret); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
 }
 
 func (c *contact) Add(details *ContactDetail, attributes core.EntityAttributes) error {
@@ -23,7 +61,7 @@ func (c *contact) Add(details *ContactDetail, attributes core.EntityAttributes) 
 		return errors.New("detail must not nil")
 	}
 
-	data, err := details.extract("query-add")
+	data, err := details.UrlValues()
 	if err != nil {
 		return err
 	}
