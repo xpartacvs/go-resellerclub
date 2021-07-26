5da63315b7a9ef7b9cf3bea9b3992e9389dbd34e
Yudi Primaputra <xpartacvs@gmail.com>
2021-07-26 10:16:55 +0700

    Add general.CountryName func

--- general/general.go
+++ general/general.go
@@ -7,10 +7,16 @@ import (
 type general struct {
 	core       core.Core
 	currencies currencyDB
+	countries  countryDB
 }
 
 type General interface {
 	CurrencyOf(iso CurrencyISO) Currency
+	CountryName(iso CountryISO) string
+}
+
+func (g *general) CountryName(iso CountryISO) string {
+	return g.countries[iso]
 }
 
 func (g *general) CurrencyOf(iso CurrencyISO) Currency {
@@ -22,8 +28,13 @@ func New(c core.Core) (General, error) {
 	if err != nil {
 		return nil, err
 	}
+	cntrs, err := fetchCountryDB(c)
+	if err != nil {
+		return nil, err
+	}
 	return &general{
 		core:       c,
 		currencies: curr,
+		countries:  cntrs,
 	}, nil
 }
